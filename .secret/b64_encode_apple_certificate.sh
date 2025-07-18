ca93093865708acf0bf483f52eec7e34bff98512
YiHeCN <yi@yihe.ee>
2025-07-19 02:21:32 +0800

    cicd: last fix for today ww

--- .secret/b64_encode_apple_certificate.sh
+++ .secret/b64_encode_apple_certificate.sh
@@ -1,5 +1,5 @@
 # Change any .p12 in the same folder to base64 txt
-for file in *.p12; do
+for file in *.p12 *p8 *.mobileprovision ; do
   if [[ -f "$file" ]]; then
     base_name="${file%.p12}"
     openssl base64 -in "$file" -out "${base_name}.txt"
